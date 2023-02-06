6e29744d2a6ff02fbcd21e98cb9755c106bed84a
Protokollmaker <82887145+Protokollmaker@users.noreply.github.com>
2023-02-06 21:22:45 +0100

    auth firebase

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,15 +3,42 @@
   import { initializeApp , getApps, getApp} from "firebase/app";
   import { getFirestore } from "firebase/firestore";
   import { browser } from "$app/environment";
+  import { getAuth, signInWithEmailAndPassword, signOut} from "firebase/auth";
  //console.log(auth);
  const firebaseApp = browser && (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
  const db = browser && getFirestore(firebaseApp);
  console.log({firebaseApp, db});
+ let email ="";
+ let password ="";
+ let user;
+ const login = () => {
+   const auth = browser && getAuth(firebaseApp);
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+ };
+ const logout = async () => {
+   const auth = getAuth(firebaseApp);
+   signOut(auth).then(() => {
+     user = null;
+   });
+ };
 </script>
 
 <section>
-  <h1>login with google</h1>
-  <button type="button">login</button>
+  <h1>email password</h1>
+  <input type="email" id="email" placehoder="email" bind:value={email} />
+  <input type="passord" id="password" placehoder="password" bind:value={password}/>
+  <button type="button" on:click={login}>login</button>
+  {#if user}
+  <button type="button" on:click={logout}>logout</button>
+  {/if}
 </section>
 
 <style>
